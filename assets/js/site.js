7ac2e14e68727c21b84e99f3842aeca24aeb5212
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2023-09-07 14:22:00 +0200

    Løst

--- assets/js/site.js
+++ assets/js/site.js
@@ -10,7 +10,8 @@ const myfirstLetterSearchButton = document.getElementById("firstLetterSearch");
 
 myfirstLetterSearchButton.addEventListener("click", () => {
   serchMode = "firstLetterSearch";
-  console.info(myfirstLetterInput.value);
+  //console.info(myfirstLetterInput.value);
+  getRecipiesByLetter(myfirstLetterInput.value);
 });
 
 const myNameInput = document.getElementById("nameInput");
@@ -28,6 +29,7 @@ const myIdSearchButton = document.getElementById("idSearch");
 myIdSearchButton.addEventListener("click", () => {
   serchMode = "idSearch";
   console.info(myIdInput.value);
+  getRecipiesById(myIdInput.value);
 });
 
 //-------------------------------------------------------------------------------------
@@ -35,29 +37,68 @@ myIdSearchButton.addEventListener("click", () => {
 // fetch functions --------------------------------------------------------------------
 // your code goes here
 
+//Search by Name
+function getRecipiesByName(myName) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${myName}`)
+  .then((response) => response.json())
+  .then((myData) => {
+      setupResultView(myData);
+    })
+  .catch((error) => {
+      console.error(error);
+    });
+}
 
 
+//Search by letter
 
+function getRecipiesByLetter(myText) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${myText}`)
+  .then((response) => response.json())
+  .then((myData) => {
+      setupResultView(myData);
+    })
+  .catch((error) => {
+      console.error(error);
+    });
+}
+//Search by ID
 
+function getRecipiesById(myId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${myId}`)
+ .then((response) => response.json())
+ .then((myData) => {
+  //console.log(mealID);
+  console.log(myData.meals);
+ })
+}
 
-
+//53071
 
 // view code --------------------------------------------------------------------------
 
 function setupResultView(myData) {
+  let myText = "";
   switch (serchMode) {
     case "firstLetterSearch":
-      console.log(myData);
+      //console.log(myData);
+
+      myData.meals.map((myMeal) => {
+        myText += myMeal.strMeal + ", ";
+      })
+
+      myResultElement.textContent = myText;
+      break;
+
       // do view stuff with the data here
       break;
 
     case "nameSearch":
       console.log(myData.meals);
-      let myText = "";
 
       myData.meals.map((myMeal) => {
         myText += myMeal.strMeal + ", ";
-      });
+      })
 
       myResultElement.textContent = myText;
       break;
